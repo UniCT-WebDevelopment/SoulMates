5dbb50d71664904c8b55284b252c2566d2bfec5c
MirkoRusso <mrk_russo@hotmail.it>
2024-09-01 19:53:32 +0200

    fix repo

---
+++ socket/websocket/socket_server.js
@@ -0,0 +1,65 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const Message = require('./models/Message');  // Assicurati che il path sia corretto
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/soulmates", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+let users = {};
+
+io.on("connection", async (socket) => {
+  socket.on('register', (userId) => {
+    users[userId] = socket.id;
+    console.log(`User con ID ${userId} registered with socket ID ${socket.id}`);
+  });
+
+  socket.on('sendMessage', async ({ senderId, receiverId, message }) => {
+    const receiverSocketId = users[receiverId];
+    
+    // Salva il messaggio nel database
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    try {
+      await newMessage.save(); 
+      console.log('Messaggio salvato nel DB:', newMessage);
+
+      if (receiverSocketId) {
+        io.to(receiverSocketId).emit('receiveMessage', {
+          senderId,
+          message,
+        });
+      }
+    } catch (error) {
+      console.error('Errore nel salvataggio del messaggio:', error);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected: ${socket.id}`);
+    for (const [userId, socketId] of Object.entries(users)) {
+      if (socketId === socket.id) {
+        delete users[userId];
+        break;
+      }
+    }
+  });
+});
+
+httpServer.listen(5001, () => {
+  console.log("Server in ascolto sulla porta 5001");
+});
